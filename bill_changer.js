243879bad6b2924540658e73dc2afa5d17438ccd
Nikita Vasilev <vassilev.nikita@gmail.com>
2019-02-07 00:45:39 +0100

    tests

--- bill_changer.js
+++ bill_changer.js
@@ -4,14 +4,40 @@ function change(cash) {
   five = 0;
   ten = 0;
   amount = cash;
+
+  if (change < 2) {
+    return null;
+  } else {
+  
+    while (amount >= 11) {
+      amount -= 10;
+      ten +=1;
+    }
+  
+    while (amount >= 7 && amount <= 11) {
+      amount -= 5;
+      five += 1;
+    }
+  
+    while (amount >= 2 && amount <= 6) {
+      if (amount == 5) {
+        amount -= 5;
+        five += 1;
+      } else {
+        amount -= 2;
+        two += 1;
+      }
+    }
+  } 
+  return {
+    two: two,
+    five: five,
+    ten: ten
+  };
 }
 
-if (change < 2) {
-  return null;
-} else {
-
-  while (amount >= 11) {
-    amount -= 10;
-    ten +=1;
-  }
-}
+console.log(change(1)); // null
+console.log(change(42).ten); // 4
+console.log(change(42).five); // 0
+console.log(change(42).two); // 1
+console.log(change(11).ten); // 1
