d2d21ee689d0d1e16042b97095d0774130e6150c
Nikita Vasilev <vassilev.nikita@gmail.com>
2019-02-06 23:57:48 +0100

    bill changer ft

---
+++ bill_changer.js
@@ -0,0 +1,17 @@
+function change(cash) {
+  let two, five, ten, amount;
+  two = 0;
+  five = 0;
+  ten = 0;
+  amount = cash;
+}
+
+if (change < 2) {
+  return null;
+} else {
+
+  while (amount >= 11) {
+    amount -= 10;
+    ten +=1;
+  }
+}
