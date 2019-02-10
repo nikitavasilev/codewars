534c3290f5efbe62c0cb97c0155d4f27d61690a3
Nikita Vasilev <vassilev.nikita@gmail.com>
2019-02-10 21:28:36 +0100

    complete

--- is_prime.js
+++ is_prime.js
@@ -26,7 +26,21 @@ You can assume you will be given an integer input.
 You can not assume that the integer will be only positive.
 You may be given negative numbers as well (or 0).
 */
+'use strict';
 
 function isPrime(num) {
-  
+  let result = false;
+  for (let i = 2; i <= num && num > 0; i++) {
+    if (num === 2) {
+      result = true;
+    } else if (num % i === 0 && num !== i) {
+      result = false;
+      break;
+    } else {
+      result = true;
+    }
+  }
+  return result;
 }
+
+isPrime(0);
