81d579d6d762f3d0979028e5f5259852b20ec5e5
Nikita Vasilev <vassilev.nikita@gmail.com>
2019-02-12 00:09:40 +0100

    working algorithm

--- 5_kyu/rgb_to_hex.js
+++ 5_kyu/rgb_to_hex.js
@@ -15,9 +15,35 @@ rgb(255, 255, 300) // returns FFFFFF
 rgb(0,0,0) // returns 000000
 rgb(148, 0, 211) // returns 9400D3
 */
+'use strict';
 
-function rgb(r, g, b){
-  // complete this function  
+function rgb(r, g, b) {
+  const hex = [
+    '0', '1', '2', '3', '4', '5', '6', '7', '8',
+    '9', 'A', 'B', 'C', 'D', 'E', 'F']; // A = 11, F = 16
+  let rQuotient, rReminder, gQuotient,
+    gReminder, bQuotient, bReminder;
+
+  if (r > 255) {
+    r = 255;
+  } else if (g > 255) {
+    g = 255;
+  } else if (b > 255) {
+    b = 255;
+  }
+
+  rQuotient = Math.floor(r / 16);
+  rReminder = ((r / 16) % 1) * 16;
+  gQuotient = Math.floor(g / 16);
+  gReminder = ((g / 16) % 1) * 16;
+  bQuotient = Math.floor(b / 16);
+  bReminder = ((b / 16) % 1) * 16;
+
+  const result = 
+    hex[rQuotient] + hex[rReminder] +
+    hex[gQuotient] + hex[gReminder] +
+    hex[bQuotient] + hex[bReminder];
+  return result;
 }
 
 console.log(rgb(255, 255, 255)); // returns FFFFFF
