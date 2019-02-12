37b54f7a3ed089cbc9004366e627679cf41d99e2
Nikita Vasilev <vassilev.nikita@gmail.com>
2019-02-12 23:42:56 +0100

    working alorithm

--- 6_kyu/ip_validation.js
+++ 6_kyu/ip_validation.js
@@ -21,10 +21,36 @@ Invalid inputs:
 
 Note that leading zeros (e.g. 01.02.03.04) are considered invalid.
 */
+'use strict';
 
 function isValidIP(str) {
-  return false;
+  str = str.split('.');
+  const strElementLength = str.map(w => w.length);
+  const ifLeadingZero = str.map((s) => s[0]);
+  let isValid = false;
+  
+  if (str.length === 4) {
+    for (let i = 0; i < str.length; i++) {
+      if (strElementLength[i] > 1 && ifLeadingZero[i] === '0') {
+        isValid = false;
+        break;
+      } else {
+        str = str.map(Number);
+        if (str[i] > 255 || str[i] < 0) {
+          isValid = false;
+          break;
+        } else {
+          isValid = true;
+        }
+      }
+    }
+  }
+  return isValid;
 }
 
-console.log(isValidIP('1.2.3.4.5')); // not valid
-console.log(isValidIP('123.45.67.89')); // valid
+console.log(isValidIP('1.2.3.4')); // true
+console.log(isValidIP('123.45.67.89')); //true
+console.log(isValidIP('1.2.3')); // false
+console.log(isValidIP('1.2.3.4.5')); // false
+console.log(isValidIP('123.456.78.90')); // false
+console.log(isValidIP('123.045.067.089')); // false
