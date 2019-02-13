df60588b202e0f57b0a54704707b4231571bc536
Nikita Vasilev <vassilev.nikita@gmail.com>
2019-02-13 02:33:32 +0100

    passing sample tests

--- 6_kyu/ip_validation.js
+++ 6_kyu/ip_validation.js
@@ -27,24 +27,19 @@ function isValidIP(str) {
   let string = str.split('.');
   const strElementLength = string.map(w => w.length);
   const ifLeadingZero = string.map((s) => s[0]);
-  let isValid = false;
+  let isValid = true;
   
-  if (str.match(/^[0-9.]+$/) == null) {
-    return false;
-  } else if (string.length === 4) {
-    for (let i = 0; i < string.length; i++) {
+  for (let i = 0; i < string.length; i++) {
+    if (str.match(/^[0-9.]+$/) === null || string[i] === '') {
+      isValid = false;
+    } else if (string.length === 4) {
       if (strElementLength[i] > 1 && ifLeadingZero[i] === '0') {
         isValid = false;
-        break;
-      } else {
-        string = string.map(Number);
-        if (string[i] > 255 || string[i] < 0) {
-          isValid = false;
-          break;
-        } else {
-          isValid = true;
-        }
+      } else if (string[i] > 255 || string[i] < 0) {
+        isValid = false;
       }
+    } else {
+      isValid = false;
     }
   }
   return isValid;
@@ -57,3 +52,20 @@ console.log(isValidIP('1.2.3.4.5')); // false
 console.log(isValidIP('123.456.78.90')); // false
 console.log(isValidIP('123.045.067.089')); // false
 console.log(isValidIP('abc.def.ghi.jkl')); // false
+console.log(isValidIP('74.9..243')); // false
+console.log(isValidIP('.108.162.196')); // false
+console.log(isValidIP('165.27..199')); // false
+console.log(isValidIP('.251.234.23')); // false
+console.log(isValidIP('78..199.213')); // false
+console.log(isValidIP('194.167.14.')); // false
+console.log(isValidIP('.156.250.247')); // false
+console.log(isValidIP('.12.208.61')); // false
+console.log(isValidIP('.106.241.210')); // false
+console.log(isValidIP('246..35.241')); // false
+console.log(isValidIP('0.0.0.0')); // true
+
+// Savage version:
+
+function isValidIP(str) {
+  return /^(([1-9]?\d|1\d\d|2[0-4]\d|25[0-5])(\.(?!$)|$)){4}$/.test(str);
+}
