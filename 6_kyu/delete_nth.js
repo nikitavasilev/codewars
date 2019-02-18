ed0c45a830abeba114121d53bd4ae9d75e6186bf
Nikita Vasilev <vassilev.nikita@gmail.com>
2019-02-18 23:11:51 +0100

    for loop

--- 6_kyu/delete_nth.js
+++ 6_kyu/delete_nth.js
@@ -32,8 +32,14 @@ deleteNth ([1,1,1,1],2) // return [1,1]
 deleteNth ([20,37,20,21],1) // return [20,37,21]
 */
 
-function deleteNth(arr,n) {
-
+function deleteNth(arr, n) {
+  let array = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (!array.includes(arr[i])) {
+      array.push(arr[i]);
+    }
+  }
+  return array;
 }
 
 console.log(deleteNth([1,1,1,1], 2)); // [1,1]
