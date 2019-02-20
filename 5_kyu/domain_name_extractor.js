970c9218ba29312271f2c18230369c98563bc657
Nikita Vasilev <vassilev.nikita@gmail.com>
2019-02-20 01:09:46 +0100

    pass sample tests

--- 5_kyu/domain_name_extractor.js
+++ 5_kyu/domain_name_extractor.js
@@ -12,11 +12,27 @@ domainName("https://www.cnet.com") == "cnet"
 */
 
 function domainName(url) {
+  let arr;
 
+  if (url.includes('http://')) {
+    url = url.slice(7);
+  } else if (url.includes('https://')) {
+    url = url.slice(8);
+  }
+  arr = url.split('.');
+
+  if (arr[0] === 'www') {
+    return arr[1];
+  } else {
+    return arr[0];
+  }
 }
 
 console.log(domainName('http://google.com')); // google
 console.log(domainName('http://google.co.jp')); // google
 console.log(domainName('www.xakep.ru')); // xakep
 console.log(domainName('https://youtube.com')); // youtube
+console.log(domainName('https://www.youtube.com')); // youtube
 console.log(domainName('http://github.com/nikitavasilev/codewars')); // github
+console.log(domainName('http://www.zombie-bites.com')); // zombie-bites
+console.log(domainName('https://www.cnet.com')); // cnet
