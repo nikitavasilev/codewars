110100831a06774283987c5424b98db7d1895b32
Nikita Vasilev <vassilev.nikita@gmail.com>
2019-02-25 23:24:51 +0100

    tests

--- 4_kyu/range_extraction.js
+++ 4_kyu/range_extraction.js
@@ -22,8 +22,14 @@ solution([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20
 'use strict';
 
 function solution(list) {
-
+  for (let i = 0; i < list[list.length - 2]; i++) {
+    for (let j = 1; j < list.length; j++) {
+      if (list[i] / list[j]) {
+        
+      }
+    }
+  }
 }
 
 const test = [-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20];
-console.log(solution(test)); // "-6,-3-1,3-5,7-11,14,15,17-20"
+console.log(solution([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20])); // "-6,-3-1,3-5,7-11,14,15,17-20"
