b52c616f4951a2c8d0a88dc9853269788fc48a7d
Nikita Vasilev <vassilev.nikita@gmail.com>
2019-03-02 20:22:09 +0100

    test 3

--- 4_kyu/range_extraction.js
+++ 4_kyu/range_extraction.js
@@ -24,7 +24,7 @@ solution([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20
 function solution(list) {
   for (let i = 0; i < list[list.length - 2]; i++) {
     for (let j = 1; j < list.length; j++) {
-      if (list[i] / list[j]) {
+      if ((list[i] / list[j]) === list[i]) {
       
       }
     }
