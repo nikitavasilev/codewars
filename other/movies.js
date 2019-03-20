01265a9ed1fa86db952deaa1ab0c1512e4967f43
Nikita Vasilev <vassilev.nikita@gmail.com>
2019-03-20 22:04:38 +0100

    obj string selector

---
+++ other/movies.js
@@ -0,0 +1,18 @@
+const movies = {
+  name: 'E.T',
+  releaseYear: 1987,
+  director: 'Steven Spielberg',
+  imdbRating: 8.9
+};
+
+function returnNameAndDirector (obj) {
+  const valuesArr = [];
+  for (let key in obj) {
+    if (typeof obj[key] === 'string') {
+      valuesArr.push(obj[key]);
+    }
+  }
+  return valuesArr.join(' ');
+};
+
+console.log(returnNameAndDirector(movies));
